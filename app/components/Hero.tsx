9b6eeb8f832458479a4d5158554c636ddb0d64c0
Emiliano Cisterna <emigrandpa@gmail.com>
2026-05-13 16:24:52 -0300

    Update resume links

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,13 +5,13 @@ export default function Hero({ lang }: { lang: 'en' | 'es' }) {
       greeting: "Hi! I'm EMILIANO CISTERNA",
       role: "> Systems Engineer",
       resumeBtn: "Download Resume",
-      resumeLink: "https://drive.google.com/file/d/1MlM2OmKJYSY6pm1m4Is_0m8yY9BkSG_S/view?usp=drive_link"
+      resumeLink: "https://drive.google.com/file/d/1DmKKegSXhM25TjKGOqDIUkXQiISuHNBi/view?usp=sharing"
     },
     es: {
       greeting: "¡Hola! Soy EMILIANO CISTERNA",
       role: "> Ingeniero en Sistemas",
       resumeBtn: "Descargar CV",
-      resumeLink: "https://drive.google.com/file/d/12WiiZPgn6XiGc5wVDa5WosXDB70Mz5_j/view?usp=drive_link"
+      resumeLink: "https://drive.google.com/file/d/1F5Iybn7t4zLjmZh6M_JixZdpLIBv0swt/view?usp=sharing"
     }
   };
 
